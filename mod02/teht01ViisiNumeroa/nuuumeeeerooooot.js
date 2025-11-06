63ac65618155f30dabb5fb23ea5b2fce4b082313
funkykong1 <vilho.valkeala@gmail.com>
2025-11-06 18:35:41 +0200

    ei mitään nähtävää kaikki oli tehty ajallaan :-)

---
+++ mod02/teht01ViisiNumeroa/nuuumeeeerooooot.js
@@ -0,0 +1,29 @@
+// t vilho
+'use strict';
+
+let numerot = [];
+// dokumentin lista
+const lista = document.getElementById("lista");
+// lol \n toimii edelleen
+numerot.push(parseInt(prompt(`Anna viisi numeroa niin tulostan ne käänteisjärjestyksessä\n anna eka nro`)))
+numerot.push(parseInt(prompt("toka")))
+numerot.push(parseInt(prompt("kolmas")))
+numerot.push(parseInt(prompt("four")))
+numerot.push(parseInt(prompt("5")))
+
+// looppi joka alkaa vikasta indeksistä ja menee nollaa kohti :p
+for (let i = numerot.length-1; i >= 0; i--)
+{
+    let li = document.createElement("li");
+    li.textContent = numerot[i]
+    lista.appendChild(li);
+}
+
+tekstiSivulle("tadaa !!numeronne ovat :")
+
+function tekstiSivulle(txt, luokka = "output1")
+{
+    document.querySelector(`.${luokka}`).textContent = txt;
+}
+
+
