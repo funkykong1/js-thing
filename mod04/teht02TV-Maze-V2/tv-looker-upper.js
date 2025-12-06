dfb0b0b01be696ecf5ed930917fa51e0bd60d428
funkykong1 <vilho.valkeala@gmail.com>
2025-12-06 14:10:14 +0200

    js

--- mod04/teht02TV-Maze-V2/tv-looker-upper.js
+++ mod04/teht02TV-Maze-V2/tv-looker-upper.js
@@ -1,18 +1,20 @@
 'use strict';
-console.log('the script starts');
 
 
-async function etiTiedot() {
-
+const input = document.getElementById("query")
+const form = document.getElementById("tv_search")
+form.addEventListener('submit', async function(evt) {
+    evt.preventDefault();
     try {
-        const response = await fetch('http://127.0.0.1:3000/airport/00A');
 
-        let jason = await response.json();
-
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${input.value}`);
+        const jsonData = await response.json();
+        console.log("löydettiin seuraavanlainen ohjelma:")
+        console.log( `nimi: ${jsonData[0]["show"]["name"]}`)
+        console.log( `kuvaus: ${jsonData[0]["show"]["summary"]}`)
     } catch (error) {
         console.log(error.message);
     }
-}
+});
 
-etiTiedot();
 
--- mod04/teht03TV-Maze-V3/tv-looker-upper.js
+++ mod04/teht03TV-Maze-V3/tv-looker-upper.js
@@ -1,22 +1,51 @@
 'use strict';
-console.log('the script starts');
-const title = document.getElementById("title")
-const desc = document.getElementById("desc")
-const pic = document.getElementById("pic")
 
-async function etiTiedot() { // asynchronous function is defined by the async keyword
+'use strict';
 
-    try {                                               // error handling: try/catch/finally
-        const response = await fetch('http://127.0.0.1:3000/airport/00A');    // starting data download, fetch returns a promise which contains an object of type 'response'
-        let jason = await response.json()[0];
-        title.textContent = jason[0]["show"]["name"]
-        desc.textContent = jason[0]["show"]["summary"]
 
-    title.textContent = jason
+const input = document.getElementById("query")
+const form = document.getElementById("tv_search")
+const article_div = document.getElementById("results")
+form.addEventListener('submit', async function(evt) {
+    evt.preventDefault();
+    try {
+
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${input.value}`);
+        const jsonData = await response.json();
+
+        article_div.innerHTML = ""
+
+        for (let i = 0; i < jsonData.length; i++)
+        {
+            // en halua vahingossa hakea 300 vastausta
+            if (i === 20)
+                break;
+
+            const article = document.createElement("article")
+            const title = document.createElement("h2")
+            const desc = document.createElement("div")
+            const pic = document.createElement("img")
+            const link = document.createElement("a")
+            title.textContent = jsonData[i].show.name
+            link.href = jsonData[i].show.url
+            link.textContent = "Lisätietoja"
+            desc.innerHTML += jsonData[i].show.summary
+            pic.src = jsonData[i].show.image.medium ? jsonData[i].show.image.medium : "https://placehold.co/210x295?text=Not%20Found"
+
+            article.appendChild(title)
+            article.appendChild(pic)
+            article.appendChild(desc)
+            article.appendChild(link)
+
+            article_div.appendChild(article)
+
+
+        }
+
     } catch (error) {
         console.log(error.message);
     }
-}
+});
 
-etiTiedot();
+
 
